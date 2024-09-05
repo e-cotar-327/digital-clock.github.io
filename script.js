be6bd5a71d36b875d3585f041bc2925ad25613cd
Elena Cotar <e_cotar@yahoo.com>
2024-09-05 12:10:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+calculateTime = () => {
+  // Creăm un nou obiect Date
+  var date = new Date();
+  // Accesăm numărul zilei şi stocăm valoarea în variabilă
+  var dayNumber = date.getDay();
+  // Accesăm ora şi stocăm valoarea în variabilă
+  var hour = date.getHours();
+  // Accesăm minutele zilei şi stocăm valoarea în variabilă
+  var minute = date.getMinutes();
+  // Setăm ora la formatul AM/PM
+  var ampm = hour >= 12 ? 'PM' : 'AM';
+  // Creăm un array cu zilele săptămânii ca să le putem accesa ulterior prin numărul zilei curente
+  var dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  hour = hour % 12;
+  hour = hour ? hour : '12';
+  hour = hour < 10 ? '0' + hour : hour;
+  minute = minute < 10 ? '0' + minute : minute;
+
+  // Targhetăm elementele necesare în pagină şi le completăm cu datele corespunzătoare
+  // - Ziua săptămânii
+  document.querySelector("#day").textContent = dayNames[dayNumber];
+  // - Ora
+  document.querySelector("#hour").textContent = hour;
+  // - Minutele
+  document.querySelector("#minute").textContent = minute;
+  // - AM/PM
+  document.querySelector("#ampm").textContent = ampm;
+
+  // - - - - - B O N U S   P A R T - - - - - -
+  // Accesăm data curentă a lunii şi stocăm valoarea în variabilă
+  var dateToday = date.getDate();
+  // Creăm un array cu lunile anului ca să le putem accesa ulterior prin numărul lunii curente
+  var monthNames = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
+  // Accesăm luna curentă a anului şi stocăm valoarea în variabilă
+  var monthNumber = date.getMonth();
+  // Accesăm anul curent şi stocăm valoarea în variabilp
+  var year = date.getFullYear();
+
+  // Targhetăm elementele necesare în pagină şi le completăm cu datele corespunzătoare
+  // - Data 
+  document.querySelector("#date").textContent = dateToday;
+  // - Luna
+  document.querySelector("#month").textContent = monthNames[monthNumber];
+  // - Anul
+  document.querySelector("#year").textContent = year;
+
+
+  // Element necesar - ne permite să actualizăm toate datele
+  setTimeout(calculateTime, 200);
+}
+
+// Apelăm funcţia la evenimentul de window load
+window.addEventListener('load', calculateTime);
